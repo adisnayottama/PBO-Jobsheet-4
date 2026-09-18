be997698efb71d4347c87c447718d56238f92785
adisnayottama <adisnayottama1@gmail.com>
2026-09-18 20:19:36 +0700

    Menggunakan Bank untuk mengelola dua Account

--- src/id/ac/polinema/Main.java
+++ src/id/ac/polinema/Main.java
@@ -2,8 +2,22 @@ package id.ac.polinema;
 
 public class Main {
     public static void main(String[] args) {
-        Account acc = new Account("A001", "Nadia", 500000);
-        acc.withdraw(150000);
-        acc.printInfo();
+        Customer customer1 = new Customer("Nadia", "0812-0000-0001");
+        Account acc1 = new Account("A001", customer1, 500000);
+        acc1.withdraw(150000);
+        acc1.printInfo();
+
+        Customer customer2 = new Customer("Sari", "0812-0000-0002");
+        Account acc2 = new Account("002", customer2, 200000);
+
+        Bank bank = new Bank(10);
+        bank.addAccount(acc1);
+        bank.addAccount(acc2);
+        bank.printAllAccount();
+
+        Account found = bank.findAccount("A002");
+        if (found != null) {
+            found.printInfo();
+        }
     }
 }
