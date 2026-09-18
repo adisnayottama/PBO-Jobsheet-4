d8716b8df9e37936fa1dcb107e40694039462d1b
adisnayottama <adisnayottama1@gmail.com>
2026-09-18 20:14:37 +0700

    awal

---
+++ src/id/ac/polinema/Bank.java
@@ -0,0 +1,33 @@
+package id.ac.polinema;
+
+public class Bank {
+    private  Account[] accounts;
+    private int count;
+
+    public Bank(int capacity) {
+        accounts = new Account[capacity];
+        count = 0;
+    }
+
+    public boolean addAccount(Account account) {
+        if (count >= accounts.length) {
+            return false;
+        }
+        accounts[count] = account;
+        count++;
+        return true;
+    }
+
+    public Account findAccount(String accountNumber){
+        for (int i = 0; i < count; i++) {
+            if (accounts[i].getAccountNumber().equals(accountNumber)) {
+            }
+        }
+        return null;
+    }
+    public void printAllAccount() {
+        for (int i = 0; i < count; i++) {
+            accounts[i].printInfo();
+        }
+    }
+}
